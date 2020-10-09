5085e6535761907c038a5d0bc34469569df974ae
thedoodler <ramprakashsmail@gmail.com>
2020-10-09 19:53:20 +0530

    converted tsv to csv

--- convert_tsv_to_csv.py
+++ convert_tsv_to_csv.py
@@ -2,3 +2,15 @@ import numpy as np
 import pandas as pd
 import sklearn
 import re
+
+
+
+tsv_f = open('tsunami_data.tsv', 'r')
+csv_f = open('tsunami_data.csv', 'w')
+
+for content in tsv_f:
+    content = re.sub("\t",",",content)
+    csv_f.write(content)
+
+csv_f.close()
+tsv_f.close()
